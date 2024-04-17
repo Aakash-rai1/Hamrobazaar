f38a6f122a870295914ae0aed4adb6bf2ce50954
Ashish Neupane <156031738+aneupane1@users.noreply.github.com>
2024-04-16 23:51:04 -0400

    sql changed

--- ecommerce_shopping.sql
+++ ecommerce_shopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2024 at 04:23 AM
+-- Generation Time: Apr 17, 2024 at 05:50 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,25 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'f925916e2754e5e03f75dd58a5733251');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `category`
 --
 
@@ -59,7 +40,7 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `categoryName`, `categoryDescription`, `creationDate`, `updationDate`) VALUES
-(7, 'Books', 'Self-help', '2023-04-18 07:01:34', '18-04-2023 08:38:01 AM'),
+(7, 'Books', 'Nikunj', '2023-04-18 07:01:34', '18-04-2023 08:38:01 AM'),
 (8, 'Electronic', 'Electronic products', '2023-04-18 07:01:55', NULL),
 (9, 'Furniture', 'Furniture', '2023-04-18 07:02:16', '18-04-2023 08:45:35 AM'),
 (10, 'Fashion', 'Fashion ', '2023-04-18 07:02:30', '18-04-2023 08:38:39 AM');
@@ -94,23 +75,6 @@ INSERT INTO `orders` (`id`, `userId`, `productId`, `quantity`, `orderDate`, `pay
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ordertrackhistory`
---
-
-CREATE TABLE `ordertrackhistory` (
-  `id` int(11) NOT NULL,
-  `orderId` int(11) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `remark` mediumtext DEFAULT NULL,
-  `postingDate` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `products`
 --
 
@@ -135,18 +99,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category`, `subCategory`, `productName`, `productCompany`, `productPrice`, `productPriceBeforeDiscount`, `productDescription`, `productImage1`, `productImage2`, `productImage3`, `shippingCharge`, `productAvailability`) VALUES
-(21, 4, 4, 'iPhone13 Pro Max', 'Apple', 2500, 2300, 'This is one of the greatest Iphone', '13pro.jpg', 'iPhone-13-Green.jpeg', 'back.png', 10, 'In Stock'),
-(37, 8, 15, 'iphone 14', 'Apple', 1600, 1500, 'apple', '14.jpg', '14pro.jpg', '14.jpg', 10, 'In Stock'),
-(1, 8, 15, 'iphone 15', 'Apple', 1800, 2000, 'apple', 'iphone15a.jpg', 'iphone-15-pro.jpeg', 'iphone15a.jpg', 10, 'In Stock'),
-(38, 10, 19, 'Kyrie 3', 'Nike', 90, 120, 'Third shoe of Kyrie Irving Signature shoe series', 'kyriewhite.jpg', 'samurai.jpg', 'flame.jpg', 20, 'In Stock'),
-(39, 10, 19, 'Chuck 70 Vintage', 'Converse', 90, 130, 'Old School Converse', 'black.jpg', 'white.jpg', 'black.jpg', 20, 'In Stock'),
-(40, 8, 16, 'Acer Nitro 5 2024', 'Acer', 1600, 1500, 'The latest budget gaming series from acer', 'nitro1.jpg', 'nitro2.jpg', 'nitro3.jpg', 20, 'In Stock'),
-(41, 8, 17, 'Samsung Neo QLED 8k', 'Samsung', 2550, 2650, 'Experience 8K experience', 'led2.jpg', 'led1.jpg', 'led3.jpg', 20, 'In Stock'),
-(42, 8, 16, 'Alienware Gaming', 'Alienware', 2600, 3200, 'Midrange gaming laptop', 'a1.jpg', 'a2.jpg', 'a3.jpg', 20, 'In Stock'),
-(43, 7, 14, 'Ikigai', 'ikigai', 60, 50, ' explores the Japanese concept of finding ones purpose in life by analyzing the habits and beliefs of the worlds longest-living people.', 'ikigai.jpg', 'ikigai2.jpg', 'ikigai3.jpeg', 5, 'In Stock'),
-(2, 7, 14, 'Rich Dad Poor Dad', 'Robert Kiyosaki', 20, 25, ' How the rich earn.', 'richdad.jpg', 'richdad1.jpg', 'richdad.jpg', 5, 'In Stock'),
-(18, 9, 18, 'Bed', 'Ikea', 180, 200, 'Ikea Bed.', 'bed1.jpeg', 'bed2.jpeg', 'bed3.jpeg', 5, 'In Stock'),
-(3, 9, 18, 'Cedar table', 'Ikea', 250, 320, 'Sturdy and attractive.', 'ikea1.jpg', 'ikea2.jpg', 'ikea.jpeg', 5, 'In Stock');
+(21, 4, 4, 'iphone13 pro max', 'Apple', 2500, 2300, 'Best', '13pro.jpg', 'th.jpg', 'graphite.jpg', 25, 'In Stock'),
+(37, 8, 15, 'Iphone 14', 'apple', 1600, 1500, 'apple', '14.jpg', '14pro.jpg', '14.jpg', 10, 'In Stock'),
+(38, 10, 19, 'Jordan', 'Jordan', 210, 199, 'Jordan<br>', 'j1.jpg', 'j2.jpg', 'j3.jpg', 20, 'In Stock'),
+(39, 10, 19, 'Jorden 2', 'Jordan', 160, 150, 'Jordan<br>', 'j2.jpg', 'j3.jpg', 'j2.jpg', 20, 'In Stock'),
+(40, 8, 16, 'Victus gaming', 'HP', 1600, 1500, 'Victus gaming<br>', 'v1.jpg', 'v2.jpg', 'v3.jpg', 20, 'In Stock'),
+(41, 8, 17, 'LED 7', 'Samsung', 1020, 999, 'good', 'led2.jpg', 'led1.jpg', 'led3.jpg', 20, 'In Stock'),
+(42, 8, 16, 'Aliean', 'DELL', 2600, 2500, 'DELL', 'a1.jpg', 'a2.jpg', 'a3.jpg', 20, 'In Stock'),
+(43, 7, 14, 'harry porter', 'harry porter', 60, 50, 'harry porter<br>', 'harrypotter.0.jpg', 'harrypotter.0.jpg', 'harrypotter.0.jpg', 5, 'In Stock');
 
 -- --------------------------------------------------------
 
@@ -176,8 +136,8 @@ INSERT INTO `subcategory` (`id`, `categoryid`, `subcategory`) VALUES
 (10, 5, 'Sofas'),
 (11, 5, 'Dining Tables'),
 (12, 6, 'Men Footwears'),
-(13, 7, 'Self-help'),
-(14, 7, 'Finance'),
+(13, 7, 'Jhonwick'),
+(14, 7, 'Nikunj'),
 (15, 8, 'Mobiles'),
 (16, 8, 'Laptops'),
 (17, 8, 'LED TV'),
@@ -204,7 +164,14 @@ CREATE TABLE `userlog` (
 --
 
 INSERT INTO `userlog` (`id`, `userEmail`, `userip`, `loginTime`, `logout`, `status`) VALUES
-(31, 'ashish@gmail.com', 0x3a3a3100000000000000000000000000, '2024-04-08 02:03:54', NULL, 1);
+(25, 'johndoe@gmail.com', 0x3a3a3100000000000000000000000000, '2023-04-18 05:24:11', '18-04-2023 06:56:27 AM', 1),
+(26, 'johndoe@gmail.com', 0x3a3a3100000000000000000000000000, '2023-04-18 06:27:39', '18-04-2023 08:03:39 AM', 1),
+(27, 'janesmith@gmail.com', 0x3a3a3100000000000000000000000000, '2023-04-18 06:35:08', '18-04-2023 08:05:15 AM', 1),
+(28, 'alicejohnson@gmail.com', 0x3a3a3100000000000000000000000000, '2023-04-18 06:35:59', '18-04-2023 08:06:03 AM', 1),
+(29, 'bobwilliams@gmail.com', 0x3a3a3100000000000000000000000000, '2023-04-18 06:36:51', NULL, 0),
+(30, 'bobwilliams@gmail.com', 0x3a3a3100000000000000000000000000, '2023-04-18 06:37:02', '18-04-2023 09:04:57 AM', 1),
+(31, 'ashish@gmail.com', 0x3a3a3100000000000000000000000000, '2024-04-08 02:03:54', NULL, 1),
+(32, 'ashish@gmail.com', 0x3a3a3100000000000000000000000000, '2024-04-17 03:46:29', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -234,22 +201,14 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `contactno`, `password`, `shippingAddress`, `shippingState`, `shippingCity`, `shippingPincode`, `billingAddress`, `billingState`, `billingCity`, `billingPincode`) VALUES
 (4, 'John Doe', 'johndoe@gmail.com', 6479782497, '4de518f35a2a288388de929d56fe7852', '170 clark avenue', 'ontario', 'kitchener', 395010, '170 clark avenue', 'ontario', 'kitchener', 0),
-(8, 'Ashish', 'ashish@gmail.com', 123456789, '827ccb0eea8a706c4c34a16891f84e7b', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
--- --------------------------------------------------------
+(6, 'Alice Johnson', 'alicejohnson@gmail.com', 4587982546, 'a1b89835caa6c1577a3e4acf813cf385', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, 'Bob Williams', 'bobwilliams@gmail.com', 8579651425, 'b4078c14fbcb7b3ef69a5f915a753d5b', 'blobkline', 'ontario', 'kitchener', 325412, 'blobkline', 'ontario', 'kitchener', 325412),
+(8, 'Ashish', 'ashish@gmail.com', 123456789, '827ccb0eea8a706c4c34a16891f84e7b', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'ashish', 'ashish@gmail.com', 0, 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
--- Table structure for table `wishlist`
---
-
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `category`
@@ -263,14 +222,6 @@ ALTER TABLE `category`
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `ordertrackhistory`
---
-ALTER TABLE `ordertrackhistory`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `productreviews`
 --
 -- Indexes for table `products`
 --
@@ -296,18 +247,8 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `wishlist`
---
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -321,15 +262,6 @@ ALTER TABLE `category`
 ALTER TABLE `orders`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
---
--- AUTO_INCREMENT for table `ordertrackhistory`
---
-ALTER TABLE `ordertrackhistory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `productreviews`
---
 --
 -- AUTO_INCREMENT for table `products`
 --
@@ -346,16 +278,13 @@ ALTER TABLE `subcategory`
 -- AUTO_INCREMENT for table `userlog`
 --
 ALTER TABLE `userlog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
